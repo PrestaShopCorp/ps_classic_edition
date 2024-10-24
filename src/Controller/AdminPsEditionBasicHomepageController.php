81d5273b9471e85ef3e6a9928f58a2649c4888f2
Jonathan LELIEVRE <jonathan.lelievre@prestashop.com>
2024-10-24 17:26:59 +0200

    Remove settings custom page

--- src/Controller/AdminPsEditionBasicHomepageController.php
+++ src/Controller/AdminPsEditionBasicHomepageController.php
@@ -23,10 +23,120 @@ declare(strict_types=1);
 
 namespace PrestaShop\Module\PsEditionBasic\Controller;
 
+use PrestaShop\Module\PsEditionBasic\Service\ModuleService;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class AdminPsEditionBasicHomepageController extends AdminPsEditionBasicController
 {
+    public function indexAction(): Response
+    {
+        if (intval($this->getContext()->employee->id_profile) !== 1) {
+            \Tools::redirectAdmin($this->getContext()->link->getAdminLink('AdminDashboard'));
+        }
+        $modulePsEditionBasic = $this->get('ps_edition_basic.module');
+
+        if ($this->has('PrestaShop\Module\PsAccounts\Service\PsAccountsService')) {
+            $psAccountService = $this->get('PrestaShop\Module\PsAccounts\Service\PsAccountsService');
+            $employeeAccount = $psAccountService->getEmployeeAccount();
+            $psAccountID = ($employeeAccount ? $employeeAccount->getUid() : $psAccountService->getUserUuid());
+            $psShopID = $psAccountService->getShopUuid();
+        } else {
+            $psAccountID = '';
+            $psShopID = '';
+        }
+
+        if ($this->has('PrestaShop\Module\PsAccounts\Repository\UserTokenRepository')) {
+            $userTokenRepository = $this->get('PrestaShop\Module\PsAccounts\Repository\UserTokenRepository');
+            $accountUserToken = strval($userTokenRepository->getOrRefreshToken());
+        } else {
+            $accountUserToken = '';
+        }
+
+        //todo: remove all this horrible mess
+        /*$reflection = new \ReflectionClass($this->getContext()->controller);
+        $method = $reflection->getMethod('getTabs');
+        $method->setAccessible(true);
+        $tabs = $method->invoke($this->getContext()->controller);*/
+        $tabs = [];
+
+
+        /* ----------------------- Allow auto install account ---------------------- */
+        $accountsFacade = null;
+        $accountsService = null;
+        try {
+            $accountsInstaller = $this->get('ps_edition_basic.ps_accounts.installer');
+            $accountsInstaller->install();
+            $accountsFacade = $this->get('ps_edition_basic.ps_accounts.facade');
+            \Media::addJsDef([
+                'contextPsAccounts' => $accountsFacade->getPsAccountsPresenter()
+                    ->present($modulePsEditionBasic->name),
+            ]);
+            $accountsService = $accountsFacade->getPsAccountsService();
+        } catch (\Exception $e) {
+            // Todo logs ?
+        }
+
+        /**
+         * @var string|string[]
+         */
+        $shopCountry = $this->getContext()->country->iso_code;
+        if (is_array($shopCountry)) { // Country might be an array
+            $shopCountry = $shopCountry[array_key_first($shopCountry)] ?? '';
+        }
+        $shopCountry = strtolower($shopCountry);
+
+        /** @var ModuleService $moduleService */
+        $moduleService = $this->get('PrestaShop\Module\PsEditionBasic\Service\ModuleService');
+
+        $callBackModuleUrl = $this->buildAdminUrl('ps_edition_basic_call_back');
+        $setupGuideApiUrl = $this->buildAdminUrl('ps_edition_basic_setup_guide_api_index');
+        $setupGuideApiUrlEdit = $this->buildAdminUrl('ps_edition_basic_setup_guide_api_edit');
+        $setupGuideApiUrlModalHidden = $this->buildAdminUrl('ps_edition_basic_setup_guide_api_modal_hidden');
+        $cacheClearApiUrl = $this->buildAdminUrl('ps_edition_basic_clean_mbo_cache');
+        $getSubscriptionApiUrl = $this->buildAdminUrl('ps_edition_basic_get_subscription');
+        $psAcademyApiUrl = $this->buildAdminUrl('ps_edition_basic_ps_academy');
+        try {
+            $smbEditionManageSubscriptionApiUrl = $this->buildAdminUrl('smb_edition_manage_subscription');
+        } catch (\Exception $e) {
+            $smbEditionManageSubscriptionApiUrl = '';
+        }
+
+        return $this->render('@Modules/ps_edition_basic/views/templates/admin/homepage.html.twig', [
+            'layoutTitle' => $this->layoutTitle(),
+            'urlAccountsCdn' => $accountsService ? $accountsService->getAccountsCdn() : '',
+            'enableSidebar' => true,
+            'jsContext' => json_encode([
+                'CALL_BACK_MODULE_URL' => $callBackModuleUrl,
+                'SETUP_GUIDE_API_URL' => $setupGuideApiUrl,
+                'SETUP_GUIDE_API_URL_EDIT' => $setupGuideApiUrlEdit,
+                'SETUP_GUIDE_API_URL_MODAL_HIDDEN' => $setupGuideApiUrlModalHidden,
+                'CACHE_CLEAR_API_URL' => $cacheClearApiUrl,
+                'GET_SUBSCRIPTION_API_URL' => $getSubscriptionApiUrl,
+                'PS_EDITION_BASIC_PS_ACADEMY_API_URL' => $psAcademyApiUrl,
+                'SMB_EDITION_MANAGE_SUBSCRIPTION_API_URL' => $smbEditionManageSubscriptionApiUrl,
+                'moduleName' => $modulePsEditionBasic->displayName,
+                'moduleSlug' => $modulePsEditionBasic->name,
+                'moduleVersion' => $modulePsEditionBasic->version,
+                'moduleIsUpdatable' => $moduleService->getModuleIsUpdatable(),
+                'moduleUpdateLink' => $moduleService->getUpdateLink(),
+                'userToken' => $accountUserToken,
+                'psAccountShopID' => $psShopID,
+                'psAccountID' => $psAccountID,
+                'shopName' => (string) $this->getConfiguration()->get('PS_SHOP_NAME', ''),
+                'isShopEnabled' => (bool) $this->getConfiguration()->get('PS_SHOP_ENABLE', false),
+                'psSubscriptionID' => (string) $this->getConfiguration()->get('PS_SHOP_SUBSCRIPTION_ID', ''),
+                'callBack' => [
+                    'isCalledBack' => (bool) $this->getConfiguration()->get('PS_IS_CALLED_BACK', false),
+                ],
+                'tabs' => $this->filter_settings_tabs_recursive($tabs, PS_EDITION_BASIC_SETTINGS_WHITE_LIST, PS_EDITION_BASIC_SETTINGS_BLACK_LIST),
+                'locale' => $this->getContext()->language->iso_code,
+                'shopCountry' => $shopCountry,
+                'modulesTabs' => $this->filter_modules_tabs_recursive($tabs, array_merge(PS_EDITION_BASIC_SETTINGS_WHITE_LIST, PS_EDITION_BASIC_MENU_WHITE_LIST)),
+            ]),
+        ]);
+    }
+
     /**
      * Handle the call back requests
      *
@@ -45,5 +155,13 @@ class AdminPsEditionBasicHomepageController extends AdminPsEditionBasicControlle
         } catch (\Exception $e) {
             return new JsonResponse(['error' => $e], JsonResponse::HTTP_NOT_FOUND);
         }
+    }
+
+    private function buildAdminUrl(string $routeName): string
+    {
+        $router = $this->get('router');
+        $scheme = isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], 'https://') === 0 ? 'https://' : 'http://';
+
+        return $scheme . $_SERVER['HTTP_HOST'] . $router->generate($routeName);
     }
 }
--- upgrade/upgrade-2.0.0.php
+++ upgrade/upgrade-2.0.0.php
@@ -1,12 +1,11 @@
 <?php
-
 /**
  * Copyright since 2007 PrestaShop SA and Contributors
  * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
+ * This source file is subject to the Academic Free License version 3.0
  * that is bundled with this package in the file LICENSE.md.
  * It is also available through the world-wide-web at this URL:
  * https://opensource.org/licenses/AFL-3.0
@@ -14,19 +13,21 @@
  * obtain it through the world-wide-web, please send an email
  * to license@prestashop.com so we can send you a copy immediately.
  *
- * @author    PrestaShop SA <contact@prestashop.com>
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
  * @copyright Since 2007 PrestaShop SA and Contributors
- * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
  */
-
-declare(strict_types=1);
-
-namespace PrestaShop\Module\PsEditionBasic\Controller;
-
-class AdminPsEditionBasicSettingsController extends AdminPsEditionBasicController
+function upgrade_module_2_0_0(ps_edition_basic $module): bool
 {
-    protected function layoutTitle(): string
-    {
-        return $this->trans('Settings', 'Modules.Editionbasic.Admin');
+    $result = true;
+
+    // Remove tab to former settings page
+    $id_tab = (int) \Tab::getIdFromClassName('AdminPsEditionBasicSettingsController');
+
+    $tab = new \Tab($id_tab);
+    if (\Validate::isLoadedObject($tab)) {
+        $result = $result && $tab->delete();
     }
+
+    return $result;
 }
