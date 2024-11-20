680a5fb6afd8a518f7a3025d9540c519ccd3d1b1
Nicolas Lœuillet <nicolas@loeuillet.org>
2024-11-20 11:24:50 +0100

    Update install-theme/footer.php
    Co-authored-by: Jonathan Lelievre <jonathan.lelievre@prestashop.com>

--- install-theme/footer.php
+++ install-theme/footer.php
@@ -2,11 +2,10 @@
 include_once _PS_INSTALL_PATH_ . '/theme/views/footer.php';
 ?>
 <div style="text-align: center;">
-    <?php echo $this->translator->trans('Any questions? We’re here to help. Visit the [1]%help_center_url%[/1]Help Center[2] or [1]%contact_us_url%[/1] contact us[2].', [
-        '%help_center_url%' => 'https://help-center.prestashop.com/',
-        '%contact_us_url%' => 'https://prestashop-academy.com/contact-us',
-        '[1]' => '<a href="',
-        '[/1]' => '" target="_blank" rel="noopener noreferrer">',
-        '[2]' => '</a>',
+    <?php echo $this->translator->trans('Any questions? We’re here to help. Visit the [1]Help Center[/1] or [2]contact us[/2].', [
+        '[1]' => '<a href="https://help-center.prestashop.com/" target="_blank" rel="noopener noreferrer">',
+        '[/1]' => '</a>',
+        '[2]' => '<a href="https://prestashop-academy.com/contact-us" target="_blank" rel="noopener noreferrer">',
+        '[/2]' => '</a>',
     ], 'Install'); ?>
 </div>
