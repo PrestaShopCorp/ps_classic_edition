43d0881e1431954be2c7400e9916d5f490d8d42e
Jonathan LELIEVRE <jonathan.lelievre@prestashop.com>
2024-12-02 17:22:26 +0100

    Fix obvious mistakes to make the module work

--- ps_classic_edition.php
+++ ps_classic_edition.php
@@ -38,7 +38,7 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require_once __DIR__ . '/vendor/autoload.php';
 }
 
-class ps_classic_edition_ extends Module
+class ps_classic_edition extends Module
 {
     use PrestaShop\Module\PsClassicEdition\Traits\UseHooks;
     use PrestaShop\Module\PsClassicEdition\Traits\HaveConfigurationPage;
