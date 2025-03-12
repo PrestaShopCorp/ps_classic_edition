98164151410f517f60b84392434487c5e64156b2
mattgoud <matthias.goudjil@prestashop.com>
2025-03-12 10:27:16 +0100

    style - [page-home-search-field] - fix #51 -  conflict with bootstrap class and tailwind base css : set prelight to false in tailwind config core-plugin attribute

--- vue-app/tailwind.config.js
+++ vue-app/tailwind.config.js
@@ -3,6 +3,9 @@ const { puikTailwindPreset } = require("@prestashopcorp/puik");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [puikTailwindPreset],
+  corePlugins: {
+    preflight: false,
+  },
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   variants: {
